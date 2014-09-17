a8edd65eea027a73453296a06ba19bdeda39ed03
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-09-17 21:23:07 +0200

    Fixed empty default label error

--- lib/inet_sr.c
+++ lib/inet_sr.c
@@ -104,7 +104,7 @@ static int INET_setroute(int action, int options, char **args)
        isnet = 1; break;
     case 2:
        isnet = 0; break;
-    default:
+    default:;
     }
 
     /* Fill in the other fields. */
