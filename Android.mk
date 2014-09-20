c7256341d48488caf3e3c983ec3c4b07080849d8
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-09-20 16:52:07 +0200

    Added rules to make hostname

--- Android.mk
+++ Android.mk
@@ -1,1 +1,23 @@
+MY_LOCAL_PATH := $(call my-dir)
+
+LOCAL_PATH := $(MY_LOCAL_PATH)
+
+#########################
+
 include $(call all-subdir-makefiles)
+
+LOCAL_PATH := $(MY_LOCAL_PATH)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  hostname.c
+
+LOCAL_MODULE := hostname
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
+
+LOCAL_STATIC_LIBRARIES := fixup dnet
+
+include $(BUILD_EXECUTABLE)
