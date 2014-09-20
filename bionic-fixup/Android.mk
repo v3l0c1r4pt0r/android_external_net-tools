319476b02035155d5dde90560ec1a2f6dcc11c41
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-09-20 16:38:16 +0200

    Included main directory to LOCAL_C_INCLUDES (not neccesary now but in future could cause troubles)

--- bionic-fixup/Android.mk
+++ bionic-fixup/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES :=  gethostname.c
 
 LOCAL_MODULE := fixup
 
-LOCAL_C_INCLUDES := include
+LOCAL_C_INCLUDES := include .
 
 LOCAL_CFLAGS := -O2 -g -W -Wall
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
