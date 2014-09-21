c5a92aeac113036a26190daf3288ec443743feef
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-09-21 11:51:45 +0200

    Added rules for netstat compilation

--- Android.mk
+++ Android.mk
@@ -21,3 +21,17 @@ LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 LOCAL_STATIC_LIBRARIES := fixup dnet
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  netstat.c statistics.c
+
+LOCAL_MODULE := netstat
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils # selinux # JB and newer may need this included
+
+LOCAL_STATIC_LIBRARIES := fixup net-tools dnet
+
+include $(BUILD_EXECUTABLE)
