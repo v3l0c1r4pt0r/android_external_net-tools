3bad4582130f5c72e01f70ac5abc9c4aba30e6a7
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-09-21 11:58:54 +0200

    Added rules for rest of the working tools

--- Android.mk
+++ Android.mk
@@ -8,6 +8,24 @@ include $(call all-subdir-makefiles)
 
 LOCAL_PATH := $(MY_LOCAL_PATH)
 
+#########################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  arp.c
+
+LOCAL_MODULE := arp
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
+
+LOCAL_STATIC_LIBRARIES := fixup net-tools
+
+include $(BUILD_EXECUTABLE)
+
+#########################
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  hostname.c
 
@@ -22,6 +40,24 @@ LOCAL_STATIC_LIBRARIES := fixup dnet
 
 include $(BUILD_EXECUTABLE)
 
+#########################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  ifconfig.c
+
+LOCAL_MODULE := ifconfig
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
+
+LOCAL_STATIC_LIBRARIES := fixup net-tools
+
+include $(BUILD_EXECUTABLE)
+
+#########################
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  netstat.c statistics.c
 
@@ -32,6 +68,38 @@ LOCAL_C_INCLUDES := include . lib bionic-fixup
 LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils # selinux # JB and newer may need this included
 
-LOCAL_STATIC_LIBRARIES := fixup net-tools dnet
+LOCAL_STATIC_LIBRARIES := fixup net-tools
+
+include $(BUILD_EXECUTABLE)
+
+#########################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  rarp.c
+
+LOCAL_MODULE := rarp
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
+
+LOCAL_STATIC_LIBRARIES := fixup net-tools
+
+include $(BUILD_EXECUTABLE)
+
+#########################
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  route.c
+
+LOCAL_MODULE := route
+
+LOCAL_C_INCLUDES := include . lib bionic-fixup
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -fno-strict-aliasing -include bionic-fixup/Xprotoent.h
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
+
+LOCAL_STATIC_LIBRARIES := fixup net-tools
 
 include $(BUILD_EXECUTABLE)
